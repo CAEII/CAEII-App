2a9a265348481c5381169dd1de50edc4321dd646
Noelia-Elizabeth <elizabeth.nmacleod@gmail.com>
2022-03-11 15:49:00 -0300

    [FEAT] Agrego Entidad User.

---
+++ backend/backend/src/main/java/com/caeii/backend/model/user.java
@@ -0,0 +1,42 @@
+package com.caeii.backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+public class user {
+
+    // Atributos que tendremos en la base de datos.
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Boolean consent;
+    private String linkedInProfile;
+
+    // Contructor vacío.
+    public user() {
+    }
+
+    // Constructor con todos los atributos.
+    public user(Long id, String email, String password, String firstName, String lastName, Boolean consent, String linkedInProfile) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.consent = consent;
+        this.linkedInProfile = linkedInProfile;
+    }
+}
