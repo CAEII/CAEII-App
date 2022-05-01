9f9456ec4329f5b02635cfcdb54fbaf924a98877
Noelia-Elizabeth <elizabeth.nmacleod@gmail.com>
2022-05-01 16:00:09 -0300

    [CHORE] Elimino archivos de User y Event para evitar conflictos.

--- backend/backend/src/main/java/com/caeii/backend/controller/errors/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.caeii.backend.controller.errors;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(Long id) {
-        super("Could not find User: " + id);
-    }
-}
--- backend/backend/src/main/java/com/caeii/backend/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.caeii.backend.repository;
-
-import com.caeii.backend.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
