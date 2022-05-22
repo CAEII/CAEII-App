5b21b50bb42ae70c6df86bc2d185d01a178eee66
Noelia-Elizabeth <elizabeth.nmacleod@gmail.com>
2022-05-22 16:37:42 -0300

    [FEAT] Paso el Login a React.

--- frontend/CAEII App/caeii-app/src/App.js
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (`
-    <h1>CAEII App</h1>
-    `);
-}
-
-export default App;
---
+++ frontend/caeii-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Login from "./components/login";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
