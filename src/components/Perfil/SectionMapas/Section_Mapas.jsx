42eefba478c4c7f18f5a357dd5d13565cdbec5a2
augusto <augustoantonelli88@gmail.com>
2022-07-18 00:42:57 -0300

    [CHORE] agrego comentarios al componente "section_mapas" y comento la ruta a perfil en app

--- src/components/Perfil/SectionMapas/Section_Mapas.jsx
+++ src/components/Perfil/SectionMapas/Section_Mapas.jsx
@@ -1,5 +1,5 @@
 // react
-import { useEffect, useState } from "react"
+import { useState } from "react"
 // components
 import PrimerPiso from "./Mapas/PrimerPiso(ejemplo)"
 import PlantaBaja from "./Mapas/Planta_baja"
@@ -18,31 +18,26 @@ export default function Section_Mapas({mapas}) {
     const [Sala_salida, Set_Sala_salida] = useState(valores_default.Sala_de_salida);       // Sala de salida o "estoy aqui", el valor por defecto depende del cronograma
     const [Sala_llegada, Set_Sala_llegada] = useState(valores_default.Sala_de_llegada);    // Sala de llegada o "voy a", el valor por defecto depende del cronograma   
 
-    const [DentroAuditorio, SetDentroAuditorio] = useState(true);         // estado de los botones "dentro / fuera del auditorio"
+    const [DentroAuditorio, SetDentroAuditorio] = useState(true);         // estado de los botones "dentro / fuera" del auditorio
 
     return (
         <section id="mapas">
             <div class="title_card_perfil title_mapas">
                 <h2>¿A dónde voy?</h2>
-
                 <div className="maps_button_container">
                     <button className={`maps_button_container_${DentroAuditorio}`} disabled={DentroAuditorio} onClick={() => {SetDentroAuditorio(!DentroAuditorio)}}> Dentro del auditorio </button>
                     <button className={`maps_button_container_${!DentroAuditorio}`} disabled={!DentroAuditorio} onClick={() => {SetDentroAuditorio(!DentroAuditorio)}}> Fuera del auditorio </button>
                 </div>
-
+                {/* si "DentroAuditorio" es igual a "true" muestro el select de "estoy aqui" y "voy a", si no lo es, no muestro nada*/}
                 {DentroAuditorio === true ? <Selects Mapas={Mapa} _useStates={{salida:Set_Sala_salida, llegada:Set_Sala_llegada}} valores_default={valores_default}/> : null}
-
             </div> 
-
+            {/* si "DentroAuditorio" es igual a "true" muestro los mapas nesesarios, si no lo es, muestro el iframe de google maps */}
             {DentroAuditorio === true ? <Mapas Mapas={Mapa} sala_resaltada={{salida:Sala_salida, llegada:Sala_llegada}}/>: <iframe title="googleMaps" src="https://www.google.com/maps/d/embed?mid=1c0v85K2T6yKGLi1ZnK43yhwhomSS07s&ehbc=2E312F" width="640" height="480"></iframe>}
-
-
         </section>
     )
 }
 
 function Selects({Mapas,_useStates,valores_default}) {
-
     const listas_de_salas = {           // esto deveria venir de otro lado, un json a parte quizas           
         PlantaBaja: ["Hall_Planta_Baja", "Sanitario_Planta_baja", "Restaurante", "Asensor_Planta_Baja"],
         PimerPiso: ["Hall_Pimer_Piso", "Uspallata", "Magna_central", "Nihuil", "Cacheuta"]
@@ -54,15 +49,14 @@ function Selects({Mapas,_useStates,valores_default}) {
                 <div class="select" id="salida">
                     <select onChange={(value) => _useStates.salida(value.target.value)} defaultValue={valores_default.Sala_de_salida}>
                         {
-                            Mapas.map((mapa, mapindex) => {
-                                return listas_de_salas[mapa].map((sala, salaIndex) => {
+                            Mapas.map((mapa, mapindex) => {                                                                             // Recorro el array de mapas y por cada mapa:
+                                return listas_de_salas[mapa].map((sala, salaIndex) => {                                                 // Recorro el array de salas y devuelvo un "<option>" con el valor de la sala
                                    return <option key={mapindex+"_"+salaIndex} value={sala}> {sala.replace(/_/g, " ")} </option>
                                 })
                             })
                         }
                     </select>
                 </div>
-
                 <label htmlFor="llegada" class="title_text">Voy a: </label>
                 <div class="select" id="llegada">
                     <select onChange={(value) => _useStates.llegada(value.target.value)} defaultValue={valores_default.Sala_de_llegada}>
