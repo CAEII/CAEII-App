7f445c8daef1c494418152f717be93aaabc2d823
augusto antonelli <augustoantonelli88@gmail.com>
2022-08-28 17:32:34 -0300

    kk3

--- src/components/Perfil/SectionCredencial/Credencial.jsx
+++ src/components/Perfil/SectionCredencial/Credencial.jsx
@@ -70,7 +70,7 @@ export default function Credencial({ nombre, asistencia, id}) {
             </div>
 
             {/* si la asistencia es igual o mayor a 80 muestro el cartel del certificado, si no lo es, no muestro nada */}
-            {asistencia >= 80 ? <div className="congratulations_smg_container"><span> CERTIFICADO OBTENIDO </span></div> : null}
+            {asistencia >= 80 ? <div className="congratulations_smg_container"><span> !MUCHAS GRACIAS POR ASISTIR¡ </span></div> : null}
         </section>
     )
 }
